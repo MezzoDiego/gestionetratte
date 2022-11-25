f18ace74ac518638de5e478d68839866e8df2bb5
MezzoDiego <diegomezzo90@gmail.com>
2022-11-25 16:14:46 +0100

    implementato repository

---
+++ src/main/java/it/prova/gestionetratte/repository/airbus/CustomAirbusRepositoryImpl.java
@@ -0,0 +1,21 @@
+package it.prova.gestionetratte.repository.airbus;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import it.prova.gestionetratte.model.Airbus;
+
+public class CustomAirbusRepositoryImpl implements CustomAirbusRepository{
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@Override
+	public List<Airbus> findByExample(Airbus example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/it/prova/gestionetratte/repository/tratta/CustomTrattaRepositoryimpl.java
@@ -0,0 +1,15 @@
+package it.prova.gestionetratte.repository.tratta;
+
+import java.util.List;
+
+import it.prova.gestionetratte.model.Tratta;
+
+public class CustomTrattaRepositoryimpl implements CustomTrattaRepository{
+
+	@Override
+	public List<Tratta> findByExample(Tratta example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
